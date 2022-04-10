45b66a1b177a09b9b2fc7f612f37b3af5b22f7b3
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-10 17:34:23 +0600

    create require auth and imple ment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Componant/About/About';
-import Header from './Componant/Header/Header';
-import Inventory from './Componant/Inventory/Inventory';
-import Login from './Componant/Login/Login';
-import Orders from './Componant/Orders/Orders';
-import Shop from './Componant/Shop/Shop';
-import SignUp from './Componant/SignUp/SignUp';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Componant/About/About";
+import Header from "./Componant/Header/Header";
+import Inventory from "./Componant/Inventory/Inventory";
+import Login from "./Componant/Login/Login";
+import Orders from "./Componant/Orders/Orders";
+import RequireAuth from "./Componant/RequireAuth/RequireAuth";
+import Shop from "./Componant/Shop/Shop";
+import SignUp from "./Componant/SignUp/SignUp";
 
 function App() {
   return (
     <div>
-     <Header></Header>
+      <Header></Header>
 
-     <Routes>
-      <Route path='/' element={<Shop></Shop>}></Route>
-      <Route path='/shop' element={<Shop></Shop>}></Route>
-      <Route path='/orders' element={<Orders></Orders>}></Route>
-      <Route path='/about' element={<About></About>} ></Route>
-      <Route path='/inventory' element={<Inventory></Inventory>}></Route>
-      <Route path='/login' element={<Login></Login>}></Route>
-<Route path='/singUp' element={<SignUp></SignUp>}></Route>
-     </Routes>
-     {/* <Shop></Shop> */}
+      <Routes>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        <Route path="/shop" element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route
+          path="/inventory"
+          element={
+            <RequireAuth>
+              <Inventory></Inventory>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/singUp" element={<SignUp></SignUp>}></Route>
+      </Routes>
+      {/* <Shop></Shop> */}
     </div>
   );
 }
--- src/Componant/Login/Login.js
+++ src/Componant/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link ,useNavigate} from 'react-router-dom';
+import { Link ,useLocation,useNavigate} from 'react-router-dom';
 import auth from "../../firebase.init";
 import "./Login.css";
 
@@ -8,6 +8,8 @@ const Login = () => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
 const navigate=useNavigate()
+const location=useLocation()
+const from =location.state?.from?.pathname || '/'
     const [
         signInWithEmailAndPassword,
         user,
@@ -20,8 +22,9 @@ const navigate=useNavigate()
     const handelPassword=e=>{
         setPassword(e.target.value);
     }
+
     if(user){
-navigate('/')
+navigate(from,{replace:true})
     }
     const handelUserSingIn=e=>{
         e.preventDefault();
